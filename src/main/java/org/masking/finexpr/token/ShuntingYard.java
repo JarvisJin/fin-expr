7b5cc07f98d03e6259e36c89b1c9e874525db1b7
Jarvis@mac13 <imasking@hotmail.com>
2017-07-29 11:15:50 +0800

    remove warning

--- src/main/java/org/masking/finexpr/token/ShuntingYard.java
+++ src/main/java/org/masking/finexpr/token/ShuntingYard.java
@@ -3,7 +3,6 @@ package org.masking.finexpr.token;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.Stack;
 
 import org.masking.finexpr.operator.Operator;
